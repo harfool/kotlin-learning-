4b35c97457942891f2dc1f42eeba23d6499f2ce4
harfool gujjar <110320932+harfool@users.noreply.github.com>
2025-07-16 02:12:35 +0530

    my first kotlin code

---
+++ first.kt
@@ -0,0 +1,16 @@
+fun main(){
+println("hlo kotlin")
+println("i'm start learning kotlin")
+println("i promise you i will 6th day complete mini project")
+fun sayMyName(){
+    println("harfool")
+}
+
+fun myDream(){
+    println("money")
+}
+sayMyName()
+myDream()
+
+}
+
